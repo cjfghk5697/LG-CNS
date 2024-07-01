beb7ca926159b92a70c613289155b60e6e8ddc21
sanghyeok-han <53252712+sanghyeok-han@users.noreply.github.com>
2024-07-02 00:10:43 +0900

    작업 내용 업로드 - 상혁

---
+++ vers_by_sanghyeok/myalgorithm_0628_1.py
@@ -0,0 +1,137 @@
+from util2 import *
+
+
+def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
+
+    start_time = time.time()
+
+    for r in all_riders:
+        r.T = np.round(dist_mat/r.speed + r.service_time).astype(int)
+
+    # A solution is a list of bundles
+    solution = []
+
+    #------------- Custom algorithm code starts from here --------------#
+
+    ALL_RIDERS = all_riders
+    ALL_ORDERS = all_orders
+    DIST = dist_mat
+
+    car_rider = ALL_RIDERS[2]
+    all_bundles_original = []
+
+    for ord in ALL_ORDERS:
+        new_bundle = Bundle(ALL_ORDERS, car_rider, [ord.id], [ord.id], ord.volume, DIST[ord.id, ord.id+K])
+        car_rider.available_number -= 1
+        all_bundles_original.append(new_bundle)
+
+    all_bundles = all_bundles_original.copy()
+
+    for i in range(len(all_bundles)):
+        bundle = all_bundles[i]
+
+        shop_seq = bundle.shop_seq
+
+        xs_s_sum = 0
+        ys_s_sum = 0
+
+        xs_e_sum = 0
+        ys_e_sum = 0
+
+        readytimes_sum = 0
+        deadlines_sum = 0
+
+        shop_seq_len = len(shop_seq)
+
+        for order_num in shop_seq:
+            order = ALL_ORDERS[order_num]
+
+            xs_s_sum += order.shop_lat
+            ys_s_sum += order.shop_lon
+
+            xs_e_sum += order.dlv_lat
+            ys_e_sum += order.dlv_lon
+
+            readytimes_sum += order.ready_time
+            deadlines_sum += order.deadline
+
+        xs_s_avg = xs_s_sum / shop_seq_len
+        ys_s_avg = ys_s_sum / shop_seq_len
+
+        xs_e_avg = xs_e_sum / shop_seq_len
+        ys_e_avg = ys_e_sum / shop_seq_len
+
+        readytimes_avg = readytimes_sum / shop_seq_len
+        deadlines_avg = deadlines_sum / shop_seq_len
+
+        avg_info = [xs_s_avg, ys_s_avg, xs_e_avg, ys_e_avg, readytimes_avg, deadlines_avg]
+
+        bundle.avg_info = avg_info
+
+    edges = []
+    for i in range(len(all_bundles)):
+        for j in range(i + 1, len(all_bundles)):
+            avg_info1 = all_bundles[i].avg_info
+            avg_info2 = all_bundles[j].avg_info
+
+            sx1, sy1, ex1, ey1, r1, d1 = avg_info1
+            sx2, sy2, ex2, ey2, r2, d2 = avg_info2
+
+            caluled_dist1 = get_dist_by_coords(sx1, sy1, sx2, sy2)
+            caluled_dist2 = get_dist_by_coords(ex1, ey1, ex2, ey2)
+
+            r_diff = abs(r1 - r2)
+            d_diff = abs(d1 - d2)
+
+            weight = 0
+
+            diff_score = caluled_dist1 + caluled_dist2 + r_diff * weight + d_diff * weight
+
+            edges.append((i, j, diff_score))
+
+    def find(v):
+        while v != parent[v]:
+            parent[v] = parent[parent[v]]
+            v = parent[v]
+
+        return v
+
+    def union(a, b, new_bundle):
+        if a > b:
+            a, b = b, a
+
+        parent[b] = a
+        all_bundles[a] = new_bundle
+
+    parent = list(range(K))
+    edges.sort(key=lambda x: x[2])
+
+    for bundle_num1, bundle_num2, diff_score in edges:
+        rbn1, rbn2 = find(bundle_num1), find(bundle_num2)
+
+        if rbn1 == rbn2:
+            continue
+        
+        new_bundle = try_merging_bundles4(K, DIST, ALL_ORDERS, ALL_RIDERS, all_bundles[rbn1], all_bundles[rbn2])
+        if new_bundle is not None:
+            all_bundles[rbn1].rider.available_number += 1
+            all_bundles[rbn2].rider.available_number += 1
+            new_bundle.rider.available_number -= 1
+
+            union(rbn1, rbn2, new_bundle)
+
+    parent = [find(v) for v in parent]
+
+    result_bundles = [all_bundles[v] for v in set(parent)]
+
+    solution = [
+        # rider type, shop_seq, dlv_seq
+        [bundle.rider.type, bundle.shop_seq, bundle.dlv_seq] for bundle in result_bundles
+    ]
+
+    #------------- End of custom algorithm code--------------#
+
+
+
+    return solution
+    
