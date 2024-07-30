0347e3fe0eb0b7491db36e4b93a98ee24d0b8132
2cu-1001 <137245969+2cu-1001@users.noreply.github.com>
2024-07-30 22:29:12 +0900

    Add files via upload

---
+++ 0730/han_SA_myalgorithm.py
@@ -0,0 +1,129 @@
+from han_SA_util import *
+
+
+def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
+
+    ALL_ORDERS = all_orders
+    ALL_RIDERS = all_riders
+    DIST = dist_mat
+
+    start_time = time.time()
+
+    for r in all_riders:
+        r.T = np.round(dist_mat/r.speed + r.service_time).astype(int)
+
+    # A solution is a list of bundles
+    solution = []
+
+    #------------- Custom algorithm code starts from here --------------#
+    inf = float('inf')
+
+    init_availables = [rider.available_number for rider in all_riders]
+
+    car_rider = [rider for rider in all_riders if rider.type == 'CAR'][0]
+    bike_rider = [rider for rider in all_riders if rider.type == 'BIKE'][0]
+    walk_rider = [rider for rider in all_riders if rider.type == 'WALK'][0]
+
+    init_availables = [rider.available_number for rider in all_riders]
+
+    min_init_cost = inf
+    min_init_cost_bundle = []
+    min_init_cost_rider_availables = []
+
+    # try_merging_bundles_by_dist // 모든 배달원과 모든 조합에서의 최소 거리를 보는 케이스
+    # try_merging_bundles_by_dist_walk_prefered // 위와 동일하지만 우선적으로 도보 배달원을 할당하는 케이스
+    # try_merging_bundles_by_cost // 번들 합칠 때 거리가 아닌 비용을 보는 케이스
+
+    for weight1, weight2 in [(1, -1), (1, -2), (1, -2.5), (1, -3), (1, -3.5), (1, -4)]:
+            bundles, result_rider_availables, cost = get_init_bundle_4_order_bundle_prefered_with_reassigning_riders(
+                 K, ALL_RIDERS, ALL_ORDERS, DIST, init_availables, weight1, weight2)
+
+            if cost < min_init_cost:
+                min_init_cost = cost
+                min_init_cost_bundles = bundles
+                min_init_cost_rider_availables = result_rider_availables
+
+    for rider_i in range(3):
+        ALL_RIDERS[rider_i].available_number = min_init_cost_rider_availables[rider_i]
+    all_bundles = min_init_cost_bundles
+
+    #--------------------------------------------- SA init ---------------------------------------------#
+    before_SA_cost = sum(bundle.cost for bundle in all_bundles) / K
+    print("SA start!!! cur time :", time.time() - start_time)
+    
+    hist = []
+    T = 100000000
+    delta = 0.99
+    T_final = 0.0001
+    cur_solution = deepcopy(all_bundles)
+    cur_cost = before_SA_cost
+    SA_iter_cnt = 0
+    T_mulitiplier = 0.00000001
+
+    # for bundle in all_bundles:
+    #     if len(bundle.shop_seq) > 3:
+    #         continue
+    #     make_path_optimal(bundle, rider_cnt, all_orders, all_riders) 
+    
+    #--------------------------------------------- SA init ---------------------------------------------#
+
+    #--------------------------------------------- SA iter ---------------------------------------------#
+    is_pre_decreased = False
+    
+    while True:
+        if time.time() - start_time > timelimit - 1 or T <= T_final: 
+            break    
+        SA_iter_cnt += 1
+        
+        new_solution, new_cost = make_new_solution(
+            car_rider, K, cur_solution, all_riders, all_orders, dist_mat, T, is_pre_decreased)
+        
+        if new_cost < cur_cost:
+            cur_solution = new_solution
+            cur_cost = new_cost
+            is_pre_decreased = True
+        elif new_cost == cur_cost:
+            is_pre_decreased = False
+            continue
+        elif new_cost > cur_cost:
+            E_multiplier = 0.3
+            p = math.exp((-(new_cost - cur_cost) * E_multiplier) / (T * T_mulitiplier))
+
+            print(int(new_cost), int(cur_cost), int(T), 
+                  (-(new_cost - cur_cost) * E_multiplier) / (T * T_mulitiplier), p)
+            if p > random.random():
+                print("changed")
+                cur_solution = new_solution
+                cur_cost = new_cost
+            is_pre_decreased = False
+        
+        T *= delta
+        hist.append(cur_cost)
+
+    print("SA iter cnt :", SA_iter_cnt)
+    
+    after_SA_cost = sum(bundle.cost for bundle in cur_solution) / K
+    
+    final_solution = cur_solution
+    if after_SA_cost > before_SA_cost:
+        final_solution = all_bundles
+        print("SA didn't work!!!")
+
+    for bundle in final_solution:
+        # if len(bundle.shop_seq) > 3:
+        #     continue
+        make_path_optimal(K, dist_mat, bundle, all_orders, all_riders)  
+
+    plt.plot(hist)
+    #--------------------------------------------- SA iter ---------------------------------------------#
+
+
+    #------------- End of custom algorithm code--------------#
+
+    solution = [
+            # rider type, shop_seq, dlv_seq
+            [bundle.rider.type, bundle.shop_seq, bundle.dlv_seq]
+            for bundle in final_solution
+    ]
+
+    return solution
