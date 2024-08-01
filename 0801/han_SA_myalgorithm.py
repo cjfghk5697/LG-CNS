568fd0515afcfe51c0eb987057233a3fab3fac4b
2cu-1001 <137245969+2cu-1001@users.noreply.github.com>
2024-08-01 23:44:14 +0900

    Update han_SA_myalgorithm.py

--- 0801/han_SA_myalgorithm.py
+++ 0801/han_SA_myalgorithm.py
@@ -114,23 +114,23 @@ def algorithm(K, all_orders, all_riders, dist_mat, timelimit=60):
     # try_merging_bundles_by_dist_walk_prefered // 위와 동일하지만 우선적으로 도보 배달원을 할당하는 케이스
     # try_merging_bundles_by_cost // 번들 합칠 때 거리가 아닌 비용을 보는 케이스
 
-    weight1, weight2 = 1, -1
-    bundles, result_rider_availables, cost = get_init_bundle_4_order_bundle_prefered_with_reassigning_riders(
-                 K, ALL_RIDERS, ALL_ORDERS, DIST, init_availables, weight1, weight2)
-    
-    if cost < min_init_cost:
-                min_init_cost = cost
-                min_init_cost_bundles = bundles
-                min_init_cost_rider_availables = result_rider_availables
-    
-    # for weight1, weight2 in [(1, -1), (1, -2), (1, -2.5), (1, -3), (1, -3.5), (1, -4)]:
-    #     bundles, result_rider_availables, cost = get_init_bundle_4_order_bundle_prefered_with_reassigning_riders(
+    # weight1, weight2 = 1, -1
+    # bundles, result_rider_availables, cost = get_init_bundle_4_order_bundle_prefered_with_reassigning_riders(
     #              K, ALL_RIDERS, ALL_ORDERS, DIST, init_availables, weight1, weight2)
+    
+    # if cost < min_init_cost:
+    #             min_init_cost = cost
+    #             min_init_cost_bundles = bundles
+    #             min_init_cost_rider_availables = result_rider_availables
+    
+    for weight1, weight2 in [(1, -1), (1, -2), (1, -2.5), (1, -3), (1, -3.5), (1, -4)]:
+        bundles, result_rider_availables, cost = get_init_bundle_4_order_bundle_prefered_with_reassigning_riders(
+                 K, ALL_RIDERS, ALL_ORDERS, DIST, init_availables, weight1, weight2)
 
-    #     if cost < min_init_cost:
-    #         min_init_cost = cost
-    #         min_init_cost_bundles = bundles
-    #         min_init_cost_rider_availables = result_rider_availables
+        if cost < min_init_cost:
+            min_init_cost = cost
+            min_init_cost_bundles = bundles
+            min_init_cost_rider_availables = result_rider_availables
 
     for rider_i in range(3):
         ALL_RIDERS[rider_i].available_number = min_init_cost_rider_availables[rider_i]
